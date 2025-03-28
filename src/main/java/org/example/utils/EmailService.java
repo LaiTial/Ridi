003b198c59ad371a7d9addb78130bb4a8f8467e8
LaiTial <asna90@naver.com>
2025-03-29 07:52:31 +0900

    Use Vault Server to encryption

--- src/main/java/org/example/utils/EmailService.java
+++ src/main/java/org/example/utils/EmailService.java
@@ -1,6 +1,7 @@
 package org.example.utils;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -9,11 +10,13 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class EmailService {
 
+    @Value("${spring.mail.from}")
+    private String fromAddress;
     private final JavaMailSender mailSender; // 스프링 이메일 전송기
 
     public void sendVerificationEmail(String email, String verificationCode) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("sample@naver.com");
+        message.setFrom(fromAddress);
         message.setTo(email);
         message.setSubject("이메일 인증 코드");
         message.setText("인증 코드: " + verificationCode);
